f7cdb823c47d3c8d19d1c4ab34d982eef3483171
Sam Thurman <sam.bbmgmt@gmail.com>
2019-10-29 13:37:32 -0700

    changed questions from 'confirm()' to 'prompt()'

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-'use strict';
+'use strict'
 
 var name = prompt('what is your name?');
 name = name.toLowerCase();
+document.write('Welcome, ' + name + '!')
 
-if (name === 'sam' || name === 'mercedes') {
-    document.write('Welcome back, gangsta!')
-} else {
-    document.write('Welcome, ' + name + '!')
-}
-
-var hawksFan = confirm('You are a Seahawks fan, right?')
+var hawksFan = confirm('You are a Seahawks fan, right?');
 if (hawksFan === true) {
     document.write('  Go Hawks!');
 } else {
     document.write('oh...');
+}
+
+var myAge = prompt('Do you think Sam is old enough to LEGALLY buy alcohol?');
+myAge = myAge.toLowerCase();
+if (myAge === 'yes') {
+    console.log('He wishes...');
+} else if (myAge === 'no') {
+    console.log('You are correct!');
+} else {
+    console.log('try again...');
+}
+
+var birthplace = prompt('Was Sam born in Seattle?');
+birthplace = birthplace.toLowerCase();
+if (birthplace === 'yes') {
+    console.log('Sorry, no. He was born in El Paso, TX.');
+} else if (birthplace === 'no') {
+    console.log('Correct! Sam was born in El Paso, Texas.');
+} else {
+    console.log('try again...')
 }
