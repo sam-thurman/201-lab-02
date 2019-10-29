f1a6c6277540bdb529ee745bf875ff12c6231c0c
Sam Thurman <sam.bbmgmt@gmail.com>
2019-10-29 14:29:19 -0700

    finished putting in all questions, added image to html file

--- app.js
+++ app.js
@@ -4,29 +4,51 @@ var name = prompt('what is your name?');
 name = name.toLowerCase();
 document.write('Welcome, ' + name + '!')
 
-var hawksFan = confirm('You are a Seahawks fan, right?');
-if (hawksFan === true) {
+var hawksFan = prompt('You are a Seahawks fan, right?');
+hawksFan = hawksFan.toLowerCase();
+if (hawksFan === 'yes') {
     document.write('  Go Hawks!');
+    console.log('Thank God')
 } else {
     document.write('oh...');
 }
 
-var myAge = prompt('Do you think Sam is old enough to LEGALLY buy alcohol?');
-myAge = myAge.toLowerCase();
-if (myAge === 'yes') {
+var earlAge = prompt('Do you think Earl is old enough to LEGALLY buy alcohol?');
+earlAge = earlAge.toLowerCase();
+if (earlAge === 'yes') {
     console.log('He wishes...');
-} else if (myAge === 'no') {
+} else if (earlAge === 'no') {
     console.log('You are correct!');
 } else {
     console.log('try again...');
 }
 
-var birthplace = prompt('Was Sam born in Seattle?');
+var birthplace = prompt('Was Earl born in Seattle?');
 birthplace = birthplace.toLowerCase();
 if (birthplace === 'yes') {
-    console.log('Sorry, no. He was born in El Paso, TX.');
+    console.log('Sorry, no. He was born in Portland, OR.');
 } else if (birthplace === 'no') {
-    console.log('Correct! Sam was born in El Paso, Texas.');
+    console.log('Correct! Earl was born in Portland, OR.');
 } else {
-    console.log('try again...')
+    console.log('try again...');
+}
+
+var siblings = prompt('Does Earl have any siblings?');
+siblings = siblings.toLowerCase();
+if (siblings = 'yes') {
+    console.log('Correct! He has a brother named Dewie, and a sister named Nelly!');
+} else if (siblings = 'no') {
+    console.log('Wrong! Earl has a brother named Dewie, and a sister named Nelly!');
+} else {
+    console.log('try again...');
+}
+
+var favColor = prompt('Is Earl\'s favorite color black?');
+favColor = favColor.toLowerCase();
+if (favColor = 'yes') {
+    console.log('DUHHHHH..');
+} else if (favColor = 'no') {
+    console.log('Wrong! Of course black is his favorite color');
+} else {
+    console.log('try again...');
 }
